455f243a277d64ccff52c7bd9228848ae7920586
qpalkim <qpalkim.dev@gmail.com>
2026-06-05 18:17:43 +0900

    🔨  style: 끝줄 추가 및 css 스타일 테스트

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,9 @@
+import styles from "./HomePage.module.scss";
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>랜딩 페이지</h1>
-    </main>
+    <div className={styles.container}>
+      <div>랜딩 페이지</div>
+    </div>
   )
 }
