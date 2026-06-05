f455c4e6dd12207f02bb9f4e7b8e519d0411ac56
qpalkim <qpalkim.dev@gmail.com>
2026-06-05 13:04:04 +0900

    ✨  feat: 색상 변수/폰트 크기/반응형 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import "@/styles/globals.scss";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
