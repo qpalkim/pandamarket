c6e4aafc3b2dfaa9f4f6fe62c1fcac8edc138934
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2026-06-08 11:34:29 +0900

    feat: component footer (#4)
    * ✨ feat: Footer 컴포넌트 제작
    
    * 💄 design: 스타일 코드 작성
    
    * 💄 design: 목록 태그 리셋
    
    * 🎨 style: App 내 Footer 렌더링 및 flex 스타일 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Router from "@/router/Router";
 import GuestHeader from "@/components/Header/GuestHeader";
+import Footer from "@/components/Footer/Footer";
+import styles from "./App.module.scss";
 
 export default function App() {
   return (
-    <>
+    <div className={styles.layout}>
       <GuestHeader />
-      <Router />
-    </>
+      <main className={styles.main}>
+        <Router />
+      </main>
+      <Footer />
+    </div>
   )
 }
