eafd97300e9b14f08f530268e65a085e0a72eb63
qpalkim <qpalkim.dev@gmail.com>
2026-06-09 13:00:29 +0900

    ✨ feat: 로그인 페이지 ui 제작

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+import Input from "@/components/Input/Input";
+import Button from "@/components/Button/Button";
+import OAuthButton from "@/components/OAuthButton/OAuthButton";
+import logo from "@/assets/logo/pandamarket.svg";
+import styles from "./LoginPage.module.scss";
+
 export default function LoginPage() {
   return (
-    <main>
-      <h1>로그인 페이지</h1>
-    </main>
-  )
+    <div className={styles.container}>
+      <div className={styles.logoWrapper}>
+        <Link to="/" aria-label="홈페이지 이동" className={styles.logo}>
+          <img src={logo} alt="" draggable={false} />
+          <span>판다마켓</span>
+        </Link>
+      </div>
+      <form className={styles.loginForm}>
+        <Input label="이메일" placeholder="이메일을 입력해 주세요" />
+        <Input
+          type="password"
+          label="비밀번호"
+          placeholder="비밀번호를 입력해 주세요"
+        />
+        <Button type="submit" size="lg" disabled>
+          로그인
+        </Button>
+      </form>
+      <OAuthButton />
+
+      <p className={styles.signupText}>
+        판다마켓이 처음인가요?&nbsp;
+        <Link
+          to="/signup"
+          aria-label="회원가입 페이지 이동"
+          className={styles.signupLink}
+        >
+          회원가입하기
+        </Link>
+      </p>
+    </div>
+  );
 }
