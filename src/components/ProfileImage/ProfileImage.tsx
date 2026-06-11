d48ed8ff819db11ecc22581abb597f2c831d5c18
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2026-06-11 18:01:08 +0900

    feat: component profile image (#11)
    * ✨ feat: 공통 컴포넌트 프로필 이미지 제작
    
    * 💄 design: 프로필 이미지 컴포넌트 스타일 코드 작성
    
    * 🚸 pref: 드래그 불가 추가

---
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,38 @@
+import defaultProfile from "@/assets/icons/defaultProfile.svg";
+import styles from "./ProfileImage.module.scss";
+
+type ProfileImageSize = "sm" | "lg";
+
+interface ProfileImageProps {
+  size?: ProfileImageSize;
+  src: string | null;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  clickable?: boolean;
+}
+
+export default function ProfileImage({
+  size = "sm",
+  src,
+  onClick,
+  clickable = false,
+}: ProfileImageProps) {
+  return (
+    <div
+      className={`${styles.profileImage} ${styles[size]} ${clickable ? styles.clickable : ""}`}
+      onClick={clickable ? onClick : undefined}
+      role={clickable ? "button" : undefined}
+      tabIndex={clickable ? 0 : undefined}
+      aria-label={clickable ? "사용자 프로필" : undefined}
+    >
+      <img
+        className={styles.image}
+        src={src || defaultProfile}
+        alt="사용자 프로필 이미지"
+        draggable={false}
+        onError={(e) => {
+          e.currentTarget.src = defaultProfile;
+        }}
+      />
+    </div>
+  );
+}
