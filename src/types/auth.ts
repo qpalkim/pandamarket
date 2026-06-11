651e229f89cb0bbb982996e689d144e84069f16e
qpalkim <qpalkim.dev@gmail.com>
2026-06-11 14:02:56 +0900

    ✨ feat: 회원가입 요청/응답 타입 정의

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,19 +1,37 @@
+// 공통 유저 타입
+export interface User {
+  id: number;
+  email: string;
+  image: string | null;
+  nickname: string;
+  updatedAt: string;
+  createdAt: string;
+}
+
 // 로그인 요청 타입
 export interface LoginRequest {
   email: string;
   password: string;
-};
+}
 
 // 로그인 응답 타입
 export interface LoginResponse {
-  user: {
-    id: number
-    email: string;
-    image: string;
-    nickname: string;
-    updatedAt: string;
-    createdAt: string;
-  }
+  user: User;
   accessToken: string;
   refreshToken: string;
-};
+}
+
+// 회원가입 요청 타입
+export interface SignUpReauest {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+// 회원가입 응답 타입
+export interface SignUpResponse {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
+}
