6e452b7c544fece5bbfa08cd6bfb58da8f414757
qpalkim <qpalkim.dev@gmail.com>
2026-06-11 14:02:37 +0900

    ✨ feat: 회원가입 요청 함수 제작

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,29 @@
-import type { LoginRequest, LoginResponse } from "@/types/auth";
-import { fetchClient } from "./client"
+import {
+  type LoginRequest,
+  type LoginResponse,
+  type SignUpReauest,
+  type SignUpResponse,
+} from "@/types/auth";
+import { fetchClient } from "./client";
 
-export const login = async (
-  body: LoginRequest
-) => {
+// 로그인 요청 API
+export const login = async (body: LoginRequest) => {
   return fetchClient<LoginResponse>("/auth/signIn", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(body)
-  })
+    body: JSON.stringify(body),
+  });
+};
+
+// 회원가입 요청 API
+export const signUp = async (body: SignUpReauest) => {
+  return fetchClient<SignUpResponse>("/auth/signUp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
 };
