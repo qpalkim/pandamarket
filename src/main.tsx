c1178cca2b40d39e66d898adef05bbcf91129076
qpalkim <qpalkim.dev@gmail.com>
2026-06-12 19:15:24 +0900

    🎨 style: AuthProvider 안에 App 렌더링

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "@/providers/AuthProvider.tsx";
+import App from "./App.tsx";
 import "@/styles/globals.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
+);
