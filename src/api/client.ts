ab1035e917151987fc828912ae5c6604b7b52faa
qpalkim <qpalkim.dev@gmail.com>
2026-06-12 18:33:58 +0900

    ♻️ refactor: fetchClient 요청 헤더 추가

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,22 @@
+import { ACCESS_TOKEN_KEY } from "@/constants/storage";
+
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchClient = async <T>(
   endpoint: string,
-  options?: RequestInit
+  options?: RequestInit,
 ): Promise<T> => {
-  const response = await fetch(
-    `${BASE_URL}${endpoint}`,
-    options
-  );
+  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  const response = await fetch(`${BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      ...(options?.headers ?? {}),
+      ...(accessToken && {
+        Authorization: `Bearer ${accessToken}`,
+      }),
+    },
+  });
 
   if (!response.ok) {
     const error = await response.json();
