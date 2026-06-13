fb0b5043edaba58a394b6c4de23322de1e7dee97
qpalkim <qpalkim.dev@gmail.com>
2026-06-13 11:44:42 +0900

    ✨ feat: 외부 클릭 시, 모달 닫는 커스텀 훅 제작

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function useClickOutside(
+  ref: React.RefObject<HTMLElement>,
+  handler: () => void,
+) {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) handler();
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
