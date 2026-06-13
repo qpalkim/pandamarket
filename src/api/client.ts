026ac3c86fe6ca43c512ad36e37a35251ded12c7
qpalkim <qpalkim.dev@gmail.com>
2026-06-13 13:18:31 +0900

    🎨 style: 401 상태 코드일 때, 토큰 갱신 요청 처리

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,5 @@
-import { ACCESS_TOKEN_KEY } from "@/constants/storage";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/constants/storage";
+import { refreshTokenUpdate } from "./auth";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
@@ -8,7 +9,7 @@ export const fetchClient = async <T>(
 ): Promise<T> => {
   const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
 
-  const response = await fetch(`${BASE_URL}${endpoint}`, {
+  let response = await fetch(`${BASE_URL}${endpoint}`, {
     ...options,
     headers: {
       ...(options?.headers ?? {}),
@@ -18,9 +19,37 @@ export const fetchClient = async <T>(
     },
   });
 
+  if (response.status === 401) {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY);
+
+    if (!refreshToken) {
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+      throw new Error("로그인이 만료되었습니다.");
+    }
+
+    try {
+      const tokenData = await refreshTokenUpdate({
+        refreshToken,
+      });
+
+      localStorage.setItem(ACCESS_TOKEN_KEY, tokenData.accessToken);
+
+      response = await fetch(`${BASE_URL}${endpoint}`, {
+        ...options,
+        headers: {
+          ...(options?.headers ?? {}),
+          Authorization: `Bearer ${tokenData.accessToken}`,
+        },
+      });
+    } catch {
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+      localStorage.removeItem(REFRESH_TOKEN_KEY);
+      throw new Error("로그인이 만료되었습니다.");
+    }
+  }
+
   if (!response.ok) {
     const error = await response.json();
-
     throw new Error(error.message);
   }
 
