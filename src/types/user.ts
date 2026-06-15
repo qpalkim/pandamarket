70c710cf1a187188771094292d01343f0554705d
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2026-06-15 11:39:30 +0900

    feat: product api (#17)
    * 🔧 chore: product 관련 타입 정의 및 호출 함수 구현
    
    * 🎨 style: 공통 user 타입 정의하여 개선
    
    * 🔨 chore: 공통 user 타입 확장
    
    * 🎨 style: 변경된 공통 사용자 타입 적용
    
    * 🐛 fix: 런 타임 빌드 오류 해결

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,7 @@
-// 내 정보 조회 타입
-export interface UserResponse {
+import type { BaseProduct } from "./product";
+
+// 공통 사용자 타입
+export interface BaseUser {
   id: number;
   nickname: string;
   image: string | null;
@@ -7,18 +9,9 @@ export interface UserResponse {
   updatedAt: string;
 }
 
-// 공통 상품 정보 조회 타입
-export interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  tags: string[];
-  images: string[];
-  ownerId: number;
+// 사용자 상품 목록 정보 조회 타입
+export interface UserProductListItem extends BaseProduct {
   ownerNickname: string;
-  favoriteCount: number;
-  createdAt: string;
 }
 
 // 내 정보 수정 요청 타입
@@ -27,7 +20,7 @@ export interface UserUpdateRequest {
 }
 
 // 내 정보 수정 응답 타입
-export type UserUpdateResponse = UserResponse;
+export type UserUpdateResponse = BaseUser;
 
 // 비밀번호 변경 요청 타입
 export interface PasswordUpdateRequest {
@@ -37,7 +30,7 @@ export interface PasswordUpdateRequest {
 }
 
 // 비밀번호 변경 응답 타입
-export type PasswordUpdateResponse = UserResponse;
+export type PasswordUpdateResponse = BaseUser;
 
 // 내가 등록한 상품 목록 조회 파라미터 타입
 export interface GetMyProductListParams {
@@ -49,7 +42,7 @@ export interface GetMyProductListParams {
 // 내가 등록한 상품 목록 조회 타입
 export interface GetMyProductListResponse {
   totalCount: number;
-  list: Product[];
+  list: UserProductListItem[];
 }
 
 // 내가 찜한 상품 목록 조회 파라미터 타입
@@ -62,5 +55,5 @@ export interface GetMyFavoriteProductListParams {
 // 내가 찜한 상품 목록 조회 타입
 export interface GetMyFavoriteProductListResponse {
   totalCount: number;
-  list: Product[];
+  list: UserProductListItem[];
 }
