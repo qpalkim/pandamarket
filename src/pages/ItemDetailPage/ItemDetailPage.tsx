e94d539deedacdd98ca3c8875bdcdad7787869a9
qpalkim <qpalkim.dev@gmail.com>
2026-06-20 15:31:06 +0900

    🚸 pref: 상품 및 문의 댓글 삭제 API 연결

--- src/pages/ItemDetailPage/ItemDetailPage.tsx
+++ src/pages/ItemDetailPage/ItemDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Heart } from "lucide-react";
 import type { GetProductDetailResponse } from "@/types/product";
 import type { BaseComment } from "@/types/comment";
@@ -7,33 +7,39 @@ import {
   getProductDetail,
   addLikeProduct,
   deleteLikeProduct,
+  deleteProduct,
 } from "@/api/product";
-import { getProductCommentList } from "@/api/comment";
+import { getProductCommentList, deleteComment } from "@/api/comment";
 import { useAuth } from "@/hooks/useAuth";
 import { formatTime } from "@/utils/formatTime";
 import CommentItem from "@/components/CommentItem/CommentItem";
 import ProfileImage from "@/components/ProfileImage/ProfileImage";
 import Textarea from "@/components/Textarea/Textarea";
 import Button from "@/components/Button/Button";
+import Dropdown from "@/components/Dropdown/Dropdown";
+import DeleteItemModal from "@/components/DeleteItemModal/DeleteItemModal";
 import defaultProduct from "@/assets/icons/defaultProduct.svg";
 import empty from "@/assets/icons/defaultComment.svg";
 import styles from "./ItemDetailPage.module.scss";
-import Dropdown from "@/components/Dropdown/Dropdown";
-
-const options = [
-  { label: "수정하기", onClick: () => {} },
-  { label: "삭제하기", onClick: () => {} },
-];
 
 export default function ItemDetailPage() {
   const { productId } = useParams();
 
   const { user } = useAuth();
 
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState<GetProductDetailResponse | null>(null);
   const [comments, setComments] = useState<BaseComment[]>([]);
   const [nextCursor, setNextCursor] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const options = [
+    { label: "수정하기", onClick: () => {} },
+    { label: "삭제하기", onClick: () => setIsOpen(true) },
+  ];
 
   const fetchProduct = async () => {
     if (!productId) return;
@@ -118,6 +124,29 @@ export default function ItemDetailPage() {
     }
   };
 
+  const handleDeleteProduct = async () => {
+    if (!product) return;
+
+    try {
+      await deleteProduct(product.id);
+
+      setIsOpen(false);
+      navigate("/items");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteComment = async (id: number) => {
+    try {
+      await deleteComment(id);
+
+      setComments((prev) => prev.filter((comment) => comment.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchProduct();
@@ -130,95 +159,109 @@ export default function ItemDetailPage() {
   const isMyProduct = user?.id === product.ownerId;
 
   return (
-    <div className={styles.container}>
-      <div className={styles.productWrapper}>
-        <div className={styles.imageWrapper}>
-          <img
-            src={product.images[0] ?? defaultProduct}
-            alt={product.name ?? "상품 이미지"}
-            className={styles.productImage}
-          />
-        </div>
-        <div className={styles.productInfo}>
-          <div className={styles.productHeader}>
-            <h1 className={styles.name}>{product.name}</h1>
-            {isMyProduct && <Dropdown options={options} />}
+    <>
+      <div className={styles.container}>
+        <div className={styles.productWrapper}>
+          <div className={styles.imageWrapper}>
+            <img
+              src={product.images[0] ?? defaultProduct}
+              alt={product.name ?? "상품 이미지"}
+              className={styles.productImage}
+            />
           </div>
-          <h4 className={styles.price}>{product.price.toLocaleString()}원</h4>
-          <h6 className={styles.sectionTitle}>상품 소개</h6>
-          <p className={styles.desc}>{product.description} </p>
-
-          <h6 className={styles.sectionTitle}>상품 태그</h6>
-          <div className={styles.tagWrapper}>
-            {product.tags.map((tag) => (
-              <span key={tag} className={styles.tags}>
-                #{tag}
-              </span>
-            ))}
-          </div>
-
-          <div className={styles.profile}>
-            <div className={styles.author}>
-              <ProfileImage src={null} size="lg" />
-              <div className={styles.authorInfo}>
-                <p className={styles.authorName}>{product.ownerNickname}</p>
-                <time>{formatTime(product.createdAt)}</time>
-              </div>
+          <div className={styles.productInfo}>
+            <div className={styles.productHeader}>
+              <h1 className={styles.name}>{product.name}</h1>
+              {isMyProduct && <Dropdown options={options} />}
             </div>
+            <h4 className={styles.price}>{product.price.toLocaleString()}원</h4>
+            <h6 className={styles.sectionTitle}>상품 소개</h6>
+            <p className={styles.desc}>{product.description} </p>
 
-            <div>
-              <button
-                type="button"
-                className={styles.likeButton}
-                onClick={handleLike}
-              >
-                <Heart
-                  className={styles.likeIcon}
-                  fill={product.isFavorite ? "currentColor" : "none"}
-                />
-                <span>{product.favoriteCount}</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.commentWrapper}>
-        <div className={styles.addComment}>
-          <h6 className={styles.sectionTitle}>문의하기</h6>
-          <Textarea placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다." />
-          <Button size="sm" disabled>
-            등록
-          </Button>
-        </div>
-
-        {comments.length === 0 ? (
-          <div className={styles.empty}>
-            <img src={empty} alt="" />
-            아직 문의가 없어요.
-          </div>
-        ) : (
-          <>
-            <div className={styles.commentList}>
-              {comments.map((comment) => (
-                <CommentItem key={comment.id} isMine={false} {...comment} />
+            <h6 className={styles.sectionTitle}>상품 태그</h6>
+            <div className={styles.tagWrapper}>
+              {product.tags.map((tag) => (
+                <span key={tag} className={styles.tags}>
+                  #{tag}
+                </span>
               ))}
             </div>
 
-            {nextCursor !== null && (
-              <div className={styles.commentMoreButton}>
-                <Button
-                  variant="outline"
-                  onClick={fetchMoreComments}
-                  isLoading={isLoading}
-                >
-                  댓글 더보기
-                </Button>
+            <div className={styles.profile}>
+              <div className={styles.author}>
+                <ProfileImage src={null} size="lg" />
+                <div className={styles.authorInfo}>
+                  <p className={styles.authorName}>{product.ownerNickname}</p>
+                  <time>{formatTime(product.createdAt)}</time>
+                </div>
               </div>
-            )}
-          </>
-        )}
+
+              <div>
+                <button
+                  type="button"
+                  className={styles.likeButton}
+                  onClick={handleLike}
+                >
+                  <Heart
+                    className={styles.likeIcon}
+                    fill={product.isFavorite ? "currentColor" : "none"}
+                  />
+                  <span>{product.favoriteCount}</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.commentWrapper}>
+          <div className={styles.addComment}>
+            <h6 className={styles.sectionTitle}>문의하기</h6>
+            <Textarea placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다." />
+            <Button size="sm" disabled>
+              등록
+            </Button>
+          </div>
+
+          {comments.length === 0 ? (
+            <div className={styles.empty}>
+              <img src={empty} alt="" />
+              아직 문의가 없어요.
+            </div>
+          ) : (
+            <>
+              <div className={styles.commentList}>
+                {comments.map((comment) => (
+                  <CommentItem
+                    key={comment.id}
+                    isMine={user?.id === comment.writer.id}
+                    onDelete={handleDeleteComment}
+                    {...comment}
+                  />
+                ))}
+              </div>
+
+              {nextCursor !== null && (
+                <div className={styles.commentMoreButton}>
+                  <Button
+                    variant="outline"
+                    onClick={fetchMoreComments}
+                    isLoading={isLoading}
+                  >
+                    댓글 더보기
+                  </Button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+
+      {isOpen && (
+        <DeleteItemModal
+          onClose={() => setIsOpen(false)}
+          onDelete={handleDeleteProduct}
+        />
+      )}
+    </>
   );
 }
