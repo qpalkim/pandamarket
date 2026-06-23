e0787210cb1e67f288b2b642cdc08e81790ad7df
Hwisong Kim <120151572+qpalkim@users.noreply.github.com>
2026-06-23 12:17:19 +0900

    feat: item detail page api (#28)
    * 🔧 chore: 문의 댓글 유효성 검사식 작성
    
    * ✨ feat: 문의 댓글 수정 기능 동작 구현
    
    * 💄 design: 수정 중일 떄, 스타일 코드 작성
    
    * 🎨 style: 문의 댓글 수정/삭제 로직 추가
    
    * 🐛 fix: 포맷 타임 함수 버그 수정
    
    * 🎨 style: 상품 상세/문의 댓글 영역 분리
    
    * 🚸 pref: 수정 중, 드롭다운 미표시 및 디자인 수정
    
    * ♻️ refactor: 코드 리팩토링
    
    * 🐛 fix: 타입 정의 오류 수정
    
    * 🐛 fix: 타입 정의 오류 수정
    
    * 🐛 fix: null 타입 재정의
    
    * 🚸 pref: 문단 줄바꿈 허용
    
    * 🚸 pref: 임시 401 처리
    
    * 💄 design: 버튼 스타일링

---
+++ src/pages/ItemDetailPage/CommentSection.tsx
@@ -0,0 +1,210 @@
+import { useEffect, useState } from "react";
+import type { BaseComment } from "@/types/comment";
+import {
+  getProductCommentList,
+  deleteComment,
+  addProductComment,
+  updateComment,
+} from "@/api/comment";
+import { useAuth } from "@/hooks/useAuth";
+import { validateComment } from "@/utils/validate";
+import CommentItem from "@/components/CommentItem/CommentItem";
+import Textarea from "@/components/Textarea/Textarea";
+import Button from "@/components/Button/Button";
+import empty from "@/assets/icons/defaultComment.svg";
+import styles from "./CommentSection.module.scss";
+import { Plus } from "lucide-react";
+
+const COMMENT_LIMIT = 6;
+
+export default function CommentSection({ productId }: { productId: number }) {
+  const { user } = useAuth();
+
+  const [comments, setComments] = useState<BaseComment[]>([]);
+  const [nextCursor, setNextCursor] = useState<number | null>(null);
+  const [isMoreCommentsLoading, setIsMoreCommentsLoading] = useState(false);
+
+  const [commentContent, setCommentContent] = useState("");
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+  const [commentTouched, setCommentTouched] = useState(false);
+  const [serverError, setServerError] = useState("");
+
+  const isCommentValid =
+    commentContent.trim().length > 0 && !validateComment(commentContent);
+
+  // 상품 문의 댓글 목록 함수
+  const fetchCommentList = async () => {
+    try {
+      const res = await getProductCommentList({
+        productId: Number(productId),
+        limit: COMMENT_LIMIT,
+      });
+
+      setComments(res.list);
+
+      if (res.list.length < COMMENT_LIMIT) {
+        setNextCursor(null);
+        return;
+      }
+
+      const nextRes = await getProductCommentList({
+        productId,
+        limit: 1,
+        cursor: res.nextCursor ?? undefined,
+      });
+
+      setNextCursor(nextRes.list.length > 0 ? res.nextCursor : null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 문의 댓글 더보기 함수
+  const fetchMoreComments = async () => {
+    if (nextCursor === null || isMoreCommentsLoading) return;
+
+    try {
+      setIsMoreCommentsLoading(true);
+
+      const res = await getProductCommentList({
+        productId,
+        limit: COMMENT_LIMIT,
+        cursor: nextCursor,
+      });
+
+      setComments((prev) => [...prev, ...res.list]);
+      setNextCursor(res.nextCursor);
+    } finally {
+      setIsMoreCommentsLoading(false);
+    }
+  };
+
+  // 문의 댓글 삭제 함수
+  const handleDeleteComment = async (id: number) => {
+    try {
+      await deleteComment(id);
+
+      setComments((prev) => prev.filter((comment) => comment.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 문의 댓글 등록 함수
+  const handleSubmitComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!user) {
+      alert("로그인 후, 이용 가능합니다.");
+      return;
+    }
+
+    setCommentTouched(true);
+    setServerError("");
+
+    if (!isCommentValid) return;
+
+    try {
+      setIsSubmitLoading(true);
+
+      const newComment = await addProductComment(productId, {
+        content: commentContent,
+      });
+
+      setComments((prev) => [newComment, ...prev]);
+      setCommentContent("");
+      setCommentTouched(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        setServerError(error.message);
+      } else {
+        setServerError("문의 댓글 등록을 실패했습니다.");
+      }
+    } finally {
+      setIsSubmitLoading(false);
+    }
+  };
+
+  // 문의 댓글 수정 함수
+  const handleUpdateComment = async (id: number, content: string) => {
+    try {
+      const updatedComment = await updateComment(id, {
+        content,
+      });
+
+      setComments((prev) =>
+        prev.map((comment) => (comment.id === id ? updatedComment : comment)),
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    fetchCommentList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [productId]);
+
+  return (
+    <div className={styles.commentWrapper}>
+      <form className={styles.commentForm} onSubmit={handleSubmitComment}>
+        <h6 className={styles.sectionTitle}>문의하기</h6>
+        <Textarea
+          value={commentContent}
+          onChange={(e) => {
+            setCommentContent(e.target.value);
+            setServerError("");
+          }}
+          onBlur={() => setCommentTouched(true)}
+          placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+          error={
+            serverError ||
+            (commentTouched ? validateComment(commentContent) : "")
+          }
+        />
+        <Button
+          type="submit"
+          size="sm"
+          disabled={!isCommentValid || isSubmitLoading}
+          isLoading={isSubmitLoading}
+        >
+          등록
+        </Button>
+      </form>
+
+      {comments.length === 0 ? (
+        <div className={styles.empty}>
+          <img src={empty} alt="" />
+          아직 문의가 없어요.
+        </div>
+      ) : (
+        <>
+          <div className={styles.commentList}>
+            {comments.map((comment) => (
+              <CommentItem
+                key={comment.id}
+                isMine={user?.id === comment.writer.id}
+                onUpdate={handleUpdateComment}
+                onDelete={handleDeleteComment}
+                {...comment}
+              />
+            ))}
+          </div>
+
+          {nextCursor !== null && (
+            <div className={styles.commentMoreButton}>
+              <Button
+                variant="outline"
+                onClick={fetchMoreComments}
+                isLoading={isMoreCommentsLoading}
+              >
+                <Plus className={styles.icon} /> 문의 댓글 더보기
+              </Button>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/ItemDetailPage/ProductDetail.tsx
@@ -0,0 +1,156 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Heart } from "lucide-react";
+import type { GetProductDetailResponse } from "@/types/product";
+import {
+  addLikeProduct,
+  deleteLikeProduct,
+  deleteProduct,
+  getProductDetail,
+} from "@/api/product";
+import { useAuth } from "@/hooks/useAuth";
+import { formatTime } from "@/utils/formatTime";
+import ProfileImage from "@/components/ProfileImage/ProfileImage";
+import Dropdown from "@/components/Dropdown/Dropdown";
+import DeleteItemModal from "@/components/DeleteItemModal/DeleteItemModal";
+import defaultProduct from "@/assets/icons/defaultProduct.svg";
+import styles from "./ProductDetail.module.scss";
+
+export default function ProductDetail({ productId }: { productId: number }) {
+  const navigate = useNavigate();
+
+  const { user } = useAuth();
+
+  const [product, setProduct] = useState<GetProductDetailResponse | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 좋아요 등록/삭제 함수
+  const handleLike = async () => {
+    if (!user) {
+      alert("로그인 후, 이용 가능합니다.");
+      return;
+    }
+
+    if (!product) return;
+
+    try {
+      await (product.isFavorite
+        ? deleteLikeProduct(product.id)
+        : addLikeProduct(product.id));
+
+      setProduct((prev) => {
+        if (!prev) return prev;
+
+        const isFavorite = !prev.isFavorite;
+
+        return {
+          ...prev,
+          isFavorite,
+          favoriteCount: isFavorite
+            ? prev.favoriteCount + 1
+            : Math.max(prev.favoriteCount - 1, 0),
+        };
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 상품 삭제 함수
+  const handleDeleteProduct = async () => {
+    if (!product) return;
+
+    try {
+      await deleteProduct(product.id);
+
+      setIsOpen(false);
+      navigate("/items");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // 상품 상세 정보 함수
+    const fetchProduct = async () => {
+      try {
+        const res = await getProductDetail(productId);
+        setProduct(res);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProduct();
+  }, [productId]);
+
+  if (!product) return <div>로딩 중</div>;
+
+  const options = [
+    { label: "수정하기", onClick: () => {} },
+    { label: "삭제하기", onClick: () => setIsOpen(true) },
+  ];
+
+  const isMyProduct = user?.id === product.ownerId;
+
+  return (
+    <>
+      <div className={styles.productWrapper}>
+        <div className={styles.imageWrapper}>
+          <img
+            src={product.images[0] || defaultProduct}
+            alt={product.name}
+            className={styles.productImage}
+          />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.productHeader}>
+            <h1 className={styles.name}>{product.name}</h1>
+            {isMyProduct && <Dropdown options={options} />}
+          </div>
+          <h4 className={styles.price}>{product.price.toLocaleString()}원</h4>
+          <h6 className={styles.sectionTitle}>상품 소개</h6>
+          <p className={styles.desc}>{product.description} </p>
+
+          <h6 className={styles.sectionTitle}>상품 태그</h6>
+          <div className={styles.tagWrapper}>
+            {product.tags.map((tag) => (
+              <span key={tag} className={styles.tags}>
+                #{tag}
+              </span>
+            ))}
+          </div>
+
+          <div className={styles.profile}>
+            <div className={styles.author}>
+              <ProfileImage src={null} size="lg" />
+              <div className={styles.authorInfo}>
+                <p className={styles.authorName}>{product.ownerNickname}</p>
+                <time>{formatTime(product.createdAt)}</time>
+              </div>
+            </div>
+
+            <button
+              type="button"
+              className={styles.likeButton}
+              onClick={handleLike}
+            >
+              <Heart
+                className={styles.likeIcon}
+                fill={product.isFavorite ? "currentColor" : "none"}
+              />
+              <span>{product.favoriteCount}</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <DeleteItemModal
+          onClose={() => setIsOpen(false)}
+          onDelete={handleDeleteProduct}
+        />
+      )}
+    </>
+  );
+}
